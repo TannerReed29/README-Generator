0d55060285a6cb574e38cb2bfa97e3426a0724f3
Tanner Brieger <tanner29@gmail.com>
2022-01-09 02:27:44 -0600

    created write to file function, still need to get licensing info

--- index.js
+++ index.js
@@ -11,11 +11,6 @@ const questions = [
     },
     {
         type: 'input',
-        message: 'GitHub Username: : ',
-        name: 'Git'
-    },
-    {
-        type: 'input',
         message: 'Description: ',
         name: 'Description'
     },
@@ -35,9 +30,9 @@ const questions = [
         name: 'Contribute'
     },
     {
-        type: 'input',
-        message: 'License: ',
+        type: 'list',
         name: 'License',
+        message: 'What is Your Project License?',
         choices: [
             'MIT',
             'GPL 3.0',
@@ -52,14 +47,65 @@ const questions = [
         message: 'Tests: ',
         name: 'Test'
     },
+    {
+        type: 'input',
+        message: 'GitHub Username: ',
+        name: 'Git'
+    },
+    {
+        type: 'input',
+        message: 'Email: ',
+        name: 'Email'
+    },
+    {
+        type: 'input',
+        message: 'Questions about your project: ',
+        name: 'Questions'
+    },
     
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    const title = `# ${data.Title}\n`; 
+        const description = `\n## Description \n ${data.Description} \n`;
+        const contents = 
+        `## Table Of Contents
+        [Installation](#Installation)
+        [Usage](#Usage)
+        [License](#License)
+        [Contributors](#Contributors)
+        [Tests](#Tests)
+        [Questions](#Questions)
+        `;
+        const install = `\n ## Installation \n ${data.Install}\n`;
+        const usage = `## Usage \n ${data.Usage}\n`;
+        const license = `## License \n ${data.License}\n`;
+        const contr = `## Contributors \n ${data.Contribute}\n`;
+        const test = `## Tests \n ${data.Test}\n`;
+        const questions = `## Questions \n GitHub Username: ${data.Git} GitHub Link: https://github.com/${data.Git} \n Email: ${data.Email} \n${data.Questions}`;
+
+        const genmd = title + description + contents + install + usage + license + contr + test + questions;  
+
+        fs.writeFile(fileName, genmd , (err) =>
+            err ? console.log(err) : console.log('README.md Creation Success!')
+            );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then((data) => {
+        const filename = "READMEtest.md";
+        writeToFile(filename, data);
+    });
+}
 
 // Function call to initialize app
 init();
+
+
+
+/* title of my project and sections entitled Description, 
+Table of Contents, Installation, Usage, 
+License, Contributing, Tests, and Questions*/
